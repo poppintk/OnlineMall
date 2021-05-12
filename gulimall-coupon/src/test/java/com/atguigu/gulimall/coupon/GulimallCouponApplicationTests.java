f7361d60d9831addcda6da6eb8f165d9ee8b2aa1
poppintk <youdonghoho@hotmail.com>
2021-05-12 18:28:31 -0400

    Fix the version

--- gulimall-coupon/src/test/java/com/atguigu/gulimall/coupon/GulimallCouponApplicationTests.java
+++ gulimall-coupon/src/test/java/com/atguigu/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -1,6 +1,6 @@
 package com.atguigu.gulimall.coupon;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
