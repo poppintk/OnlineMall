b593918fdddedcb28d3c7b24b7d0e2be77f68a06
poppintk <youdonghoho@hotmail.com>
2021-05-13 16:33:53 -0400

    nacos 配置中心 學習

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
@@ -16,6 +16,34 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  *  給這個xxx.properties 添加任何配置
  *  如何動態獲取配置？ 用2個注解 1） @RefreshScope 2) @Value
  *  如果配置中心和當前應用的配置都配置了相同的項，有限使用配置中心的項
+ *
+ *
+ *  細節：
+ *  1）命名空間
+ *      默認：public(保留空間)： 默認新增的所有配置都子啊public空間
+ *      1. 開發，測試， 生產 :利用命名空間來做環境隔離
+ *        注意： 在bootstrap.properties: 配置上，需要使用哪個命名空間的配置
+ *        spring.cloud.nacos.config.namespace=627bb290-7a78-49c2-845e-8fcd4edf0756
+ *      2. 每一個微服務之間互相隔離配置， 每一個微服務都創建自己的命名空間，之加載自己的命名空間
+ *  2） 配置集： 所有配置的集合
+ *
+ *  3）配置集ID： 配置文件名字
+ *      Data ID :配置文件名
+ *
+ *  4) 配置分組：
+ *      默認所有的配置集都屬於DEFAULT_GROUP：
+ *      1111. 618, 1212
+ *      spring.cloud.nacos.config.group=1111
+ *
+ * 每個微服務創建自己的命名空間， 使用配置分組來區分環境，dev, test, prod
+ *
+ * 同時加載多個命名空間
+ * 1）微服務任何配置信息，任何配置文件都可以放在配置中心
+ * 2）只需要在bootstrap.properties  中説明加載配置中心哪些配置文件即可
+ *  spring.cloud.nacos.config.ext-config[0].adta-id=mybatis.yml
+ *  spring.cloud.nacos.config.ext-config[0].group=dev
+ *  spring.cloud.nacos.config.ext-config[0].refresh=true
+ *
  */
 @EnableDiscoveryClient
 @MapperScan("com.atguigu.gulimall.coupon.dao")
