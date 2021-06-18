ace3ce6eeb8df06a857787597475edfe2d21d2c2
poppintk <youdonghoho@hotmail.com>
2021-06-17 20:20:32 -0400

    adding item page link to other page

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/SkuItemSaleAttrVo.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/SkuItemSaleAttrVo.java
@@ -2,11 +2,12 @@ package com.atguigu.gulimall.product.vo;
 
 import lombok.Data;
 import lombok.ToString;
+import java.util.List;
 
 @ToString
 @Data
 public class SkuItemSaleAttrVo {
     private Long attrId;
     private String attrName;
-    private String attrValues;
+    private List<AttrValueWithSkuIdVo> attrValues;
 }
