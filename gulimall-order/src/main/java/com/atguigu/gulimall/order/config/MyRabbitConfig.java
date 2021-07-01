2d35eec8087f516ead626f9dafb7ef0ad1a94f6b
poppintk <youdonghoho@hotmail.com>
2021-07-01 19:55:07 -0400

    rabbitMQ example

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/config/MyRabbitConfig.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/config/MyRabbitConfig.java
@@ -1,14 +1,70 @@
 package com.atguigu.gulimall.order.config;
 
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+
 @Configuration
 public class MyRabbitConfig {
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+
     @Bean
     public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    /**
+     * 定制RabbitTemplate
+     */
+    @PostConstruct //对象创建完成以后，执行这个方法
+    public void initRabbitTemplate(){
+        //设置确认回调
+        rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
+            /**
+             * ConfirmCallback
+             * 只要消息抵达broker服务器 ack 就为true
+             * @param correlationData 当前消息的唯一关联数据（这个是消息的唯一id）
+             * @param ack 消息是否成功收到
+             * @param cause 失败的原因
+             */
+            @Override
+            public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+                System.out.println("confirm... correlationData" + correlationData + " => ack " + ack + "=> cause " + cause);
+            }
+        });
+
+        /**
+         * returnCallback
+         * 只要消息抵达队列的确认回调,注意是投递失败的时候触发
+         */
+        rabbitTemplate.setReturnCallback(new RabbitTemplate.ReturnCallback() {
+            /**
+             *
+             * @param message 投递失败的消息详细信息
+             * @param replyCode 回复的状态码
+             * @param replyText 回复的文本内容
+             * @param exchange 当时这个消息发给哪个交换机
+             * @param routingKey 但是这个消息用哪个路邮件
+             */
+            @Override
+            public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+                System.out.println("confirm... message " + message +
+                        " => replyCode " + replyCode +
+                        "=> replyText " + replyText +
+                        "=> exchange " + exchange +
+                        "=> routingKey " + routingKey
+                );
+            }
+        });
     }
 }
--- gulimall-order/src/main/java/com/atguigu/gulimall/order/service/impl/OrderItemServiceImpl.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/service/impl/OrderItemServiceImpl.java
@@ -9,12 +9,13 @@ import com.atguigu.gulimall.order.service.OrderItemService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
-import java.nio.channels.Channel;
+import java.io.IOException;
 import java.util.Map;
 
 
@@ -37,6 +38,20 @@ public class OrderItemServiceImpl extends ServiceImpl<OrderItemDao, OrderItemEnt
         MessageProperties properties = message.getMessageProperties(); // 消息头
         System.out.println("接受到的消息" + message + "===>内容：" + content);
         System.out.println(channel);
+        //  开启手动ack 模式， 只要我们没有明确告诉MQ, 货物被签收。 那么消息就一直是unacked状态， 即使consumer宕机.消息也不会丢失
+        long deliveryTag = message.getMessageProperties().getDeliveryTag();//chnnel内按顺序自增
+        // 签收, 非批量模式
+        try {
+            channel.basicAck(deliveryTag, false);
+        } catch (Exception e) {
+            log.error("Error: {}", e);
+            // 第三个参数是，重新入queue的意思, false 不重新入队
+            try {
+                channel.basicNack(deliveryTag, false,true);
+            } catch (IOException ioException) {
+                log.error("Error: {}", ioException);
+            }
+        }
     }
 
 }
