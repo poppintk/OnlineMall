20bcb0c39726158854007873dd31d1c2e3536e70
poppintk <youdonghoho@hotmail.com>
2021-07-01 22:33:33 -0400

    configure html user inforamtion and spring session

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/service/impl/OrderItemServiceImpl.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/service/impl/OrderItemServiceImpl.java
@@ -32,26 +32,26 @@ public class OrderItemServiceImpl extends ServiceImpl<OrderItemDao, OrderItemEnt
         return new PageUtils(page);
     }
 
-    @RabbitListener(queues = {"hello-java-queue"})
-    public void receiveMessage(Message message, OrderReturnReasonEntity content, Channel channel) {
-        byte[] body = message.getBody(); //消息体
-        MessageProperties properties = message.getMessageProperties(); // 消息头
-        System.out.println("接受到的消息" + message + "===>内容：" + content);
-        System.out.println(channel);
-        //  开启手动ack 模式， 只要我们没有明确告诉MQ, 货物被签收。 那么消息就一直是unacked状态， 即使consumer宕机.消息也不会丢失
-        long deliveryTag = message.getMessageProperties().getDeliveryTag();//chnnel内按顺序自增
-        // 签收, 非批量模式
-        try {
-            channel.basicAck(deliveryTag, false);
-        } catch (Exception e) {
-            log.error("Error: {}", e);
-            // 第三个参数是，重新入queue的意思, false 不重新入队
-            try {
-                channel.basicNack(deliveryTag, false,true);
-            } catch (IOException ioException) {
-                log.error("Error: {}", ioException);
-            }
-        }
-    }
+//    @RabbitListener(queues = {"hello-java-queue"})
+//    public void receiveMessage(Message message, OrderReturnReasonEntity content, Channel channel) {
+//        byte[] body = message.getBody(); //消息体
+//        MessageProperties properties = message.getMessageProperties(); // 消息头
+//        System.out.println("接受到的消息" + message + "===>内容：" + content);
+//        System.out.println(channel);
+//        //  开启手动ack 模式， 只要我们没有明确告诉MQ, 货物被签收。 那么消息就一直是unacked状态， 即使consumer宕机.消息也不会丢失
+//        long deliveryTag = message.getMessageProperties().getDeliveryTag();//chnnel内按顺序自增
+//        // 签收, 非批量模式
+//        try {
+//            channel.basicAck(deliveryTag, false);
+//        } catch (Exception e) {
+//            log.error("Error: {}", e);
+//            // 第三个参数是，重新入queue的意思, false 不重新入队
+//            try {
+//                channel.basicNack(deliveryTag, false,true);
+//            } catch (IOException ioException) {
+//                log.error("Error: {}", ioException);
+//            }
+//        }
+//    }
 
 }
