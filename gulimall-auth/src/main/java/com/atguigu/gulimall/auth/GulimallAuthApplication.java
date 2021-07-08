0f40e0d9fd71f31c340b512d470e01e54ac5255a
poppintk <youdonghoho@hotmail.com>
2021-07-08 13:16:19 -0400

    adding swagger2

--- gulimall-auth/src/main/java/com/atguigu/gulimall/auth/GulimallAuthApplication.java
+++ gulimall-auth/src/main/java/com/atguigu/gulimall/auth/GulimallAuthApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 // 整合redis作为session存储
 @EnableRedisHttpSession
 @EnableFeignClients(basePackages = "com.atguigu.gulimall.auth.feign")
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableSwagger2
 public class GulimallAuthApplication {
 
     public static void main(String[] args) {
